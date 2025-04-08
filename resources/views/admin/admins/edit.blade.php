0d738b3b7726fba9a47a640e09b8cc4157bf1dce
atefAkl <atefakl80@gmail.com>
2025-04-08 23:39:13 +0300

    merging

--- resources/views/admin/admins/edit.blade.php
+++ resources/views/admin/admins/edit.blade.php
@@ -17,133 +17,148 @@
                     </h1>
 
                     <div class="row">
-                        <div class="col col-8 card mb-3">
-                            <div class="card-header">
-                                <h5 class="card-title py-2">
-                                    {{ __('Account Basic Info') }}
-                                </h5>
+                        <div class="col col-8 mb-3 p-1">
+                            <div class="card w-100">
+                                <div class="card-header">
+                                    <h5 class="card-title py-2">
+                                        {{ __('Account Basic Info') }}
+                                    </h5>
+                                </div>
+                                <form action="{{ route('update-admin-info') }}" method="POST">
+                                    <div class="card-body">
+                                        @csrf
+                                        @method('put')
+
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text" for="userName">User Name</label>
+                                            <input type="text" class=" form-control sm " name="userName" id="userName"
+                                                value="{{ old('last_name', $admin->profile) }}">
+                                        </div>
+                                        @error('userName')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text" for="email">Email</label>
+                                            <input type="email" class="form-control sm" name="email" id="email"
+                                                value="{{ old('last_name', $admin->profile) }}">
+                                        </div>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text" for="role_name">{{ __('Role') }}</label>
+                                            <select class="form-select sm" name="role_name" id="role_name"
+                                                value="{{ old('last_name', $admin->profile) }}">
+                                                <option hidden>{{ __('Select Role') }}</option>
+                                                @foreach ($roles as $role)
+                                                    <option value="{{ $role }}">{{ __('admins.' . $role) }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        @error('role_name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+
+                                    </div>
+                                    <div class="card-footer d-flex justify-content-end">
+                                        <button type=submit class="btn py-0 btn-outline-primary"><i
+                                                class="fa fa-save me-2"></i>
+                                            {{ __('Update') }}</button>
+                                    </div>
+                                </form>
                             </div>
-                            <form action="{{ route('update-admin-info') }}" method="POST">
-                                <div class="card-body">
-                                    @csrf
-                                    @method('put')
-
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="userName">User Name</label>
-                                        <input type="text" class=" form-control sm " name="userName" id="userName"
-                                            value="{{ old('last_name', $admin->profile) }}">
-                                    </div>
-                                    @error('userName')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="email">Email</label>
-                                        <input type="email" class="form-control sm" name="email" id="email"
-                                            value="{{ old('last_name', $admin->profile) }}">
-                                    </div>
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="role_name">{{ __('Role') }}</label>
-                                        <select class="form-select sm" name="role_name" id="role_name"
-                                            value="{{ old('last_name', $admin->profile) }}">
-                                            <option hidden>{{ __('Select Role') }}</option>
-                                            @foreach ($roles as $role)
-                                                <option value="{{ $role }}">{{ __('admins.' . $role) }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    @error('role_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-
-                                </div>
-                                <div class="card-footer d-flex justify-content-end">
-                                    <button type=submit class="btn py-0 btn-outline-primary"><i class="fa fa-save me-2"></i>
-                                        {{ __('Update') }}</button>
-                                </div>
-                            </form>
                         </div>
 
                         {{-- Change Password --}}
-                        <div class="col col-4 card mb-3">
-                            <div class="card-header">
-                                <h5 class="card-title py-2">{{ __('Change Password') }}</h5>
+                        <div class="col col-4 mb-3 p-1">
+                            <div class="card w-100">
+                                <div class="card-header">
+                                    <h5 class="card-title py-2">{{ __('Change Password') }}</h5>
+                                </div>
+                                <form action="{{ '' }}">
+                                    <div class="card-body">
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text"
+                                                for="current_password">{{ __('Current Password') }}</label>
+                                            <input type="password" class="form-control sm" name="current_password"
+                                                id="current_password">
+                                        </div>
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text"
+                                                for="new_password">{{ __('New Password') }}</label>
+                                            <input type="password" class="form-control sm" name="password"
+                                                id="new_password">
+                                        </div>
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text"
+                                                for="password_confirmation">{{ __('Confirm Password') }}</label>
+                                            <input type="password" class="form-control sm" name="password_confirmation"
+                                                id="password_confirmation">
+                                        </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <div class="d-flex justify-contecnt-end">
+                                            <button class="btn btn-outline-primary py-0"
+                                                type="submit">{{ __('Update Password') }}
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                            <form action="{{ '' }}">
-                                <div class="card-body">
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text"
-                                            for="current_password">{{ __('Current Password') }}</label>
-                                        <input type="password" class="form-control sm" name="current_password"
-                                            id="current_password">
-                                    </div>
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="new_password">{{ __('New Password') }}</label>
-                                        <input type="password" class="form-control sm" name="password" id="new_password">
-                                    </div>
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text"
-                                            for="password_confirmation">{{ __('Confirm Password') }}</label>
-                                        <input type="password" class="form-control sm" name="password_confirmation"
-                                            id="password_confirmation">
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-contecnt-end card-footer">
-                                    <button class="brn btn-outline-primary py-0"
-                                        type="submit">{{ __('Update Password') }}</button>
-                                </div>
-                            </form>
                         </div>
 
 
                         {{-- Profile Legal Info --}}
-                        <div class="card mb-3">
-                            <div class="card-header">
-                                <h5 class="card-title py-2">
-                                    {{ __('Profile Identity') }}
-                            </div>
-                            </h5>
-                            <form action="{{ route('update-admin-info') }}" method="POST">
-                                <div class="card-body">
-                                    @csrf
-                                    @method('put')
+                        <div class="col col-6 mb-3 p-1">
+                            <div class="card w-100">
+                                <div class="card-header">
+                                    <h5 class="card-title py-2">
+                                        {{ __('Profile Identity') }}
+                                    </h5>
+                                </div>
+                                <form action="{{ route('update-admin-info') }}" method="POST">
+                                    <div class="card-body">
+                                        @csrf
+                                        @method('put')
 
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="first_name">First Name</label>
-                                        <input type="text" class="form-control sm" name="first_name" id="first_name"
-                                            value="{{ old('first_name', $admin->profile) }}">
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text" for="first_name">First Name</label>
+                                            <input type="text" class="form-control sm" name="first_name"
+                                                id="first_name" value="{{ old('first_name', $admin->profile) }}">
+                                        </div>
+                                        @error('first_name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        <div class="input-group sm mb-2">
+                                            <label class="input-group-text" for="last_name">Last Name</label>
+                                            <input type="text" class="form-control sm" name="last_name"
+                                                id="last_name" value="{{ old('last_name', $admin->profile) }}">
+                                        </div>
+                                        @error('last_name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
-                                    @error('first_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                    <div class="input-group sm mb-2">
-                                        <label class="input-group-text" for="last_name">Last Name</label>
-                                        <input type="text" class="form-control sm" name="last_name" id="last_name"
-                                            value="{{ old('last_name', $admin->profile) }}">
+                                    <div class="card-footer">
+                                        <div class="d-flex justify-content-end">
+                                            <button type=submit class="btn py-0 btn-outline-primary"><i
+                                                    class="fa fa-save me-2"></i>
+                                                {{ __('Update') }}</button>
+                                        </div>
                                     </div>
-                                    @error('last_name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                                <div class="card-footer d-flex justify-content-end">
-                                    <button type=submit class="btn py-0 btn-outline-primary"><i
-                                            class="fa fa-save me-2"></i>
-                                        {{ __('Update') }}</button>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
