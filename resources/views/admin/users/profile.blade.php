9a54c716ec0b59373e056aa5a32696fe514020bc
Yosra Ziad <yosra.mohammed.ziad@gmail.com>
2025-04-08 21:53:56 +0300

    working profile model

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -138,127 +138,130 @@
         <div class="tab-pane fade show active" id="PersonalInfo" role="tabpanel" aria-labelledby="PersonalInfo-tab">
           <div class="profile-container ">
             <div class="profile-header">
-              <h4>Update Your Photo and Personal Details</h4>
+              <h4>Update Your  Personal Info</h4>
             </div>
 
-                <!-- Profile Picture Section -->
-                <div class="row mb-4 text-center">
-                    <div class="col-12">
-                        <img src="{{asset('assets/admin/uploads/images/avatar/avatar-04.jpg')}}" alt="Profile Picture" class="profile-picture">
-                        <div class="mt-3">
-                            <input type="file" id="profilePhoto" class="d-none">
-                            <label for="profilePhoto" class="btn btn-upload">
-                                <i class="fas fa-camera me-2"></i>Change Photo
-                            </label>
-                        </div>
-                        <small class="text-muted">JPEG or PNG, max 2MB</small>
+            <!-- Profile Picture Section -->
+            <div class="row mb-4 text-center">
+              <div class="col-12">
+                <img src="{{asset('assets/admin/uploads/images/avatar/avatar-04.jpg')}}" alt="Profile Picture" class="profile-picture">
+                <div class="mt-3">
+                  <input type="file" id="profilePhoto" class="d-none">
+                  <label for="profilePhoto" class="btn btn-upload">
+                    <i class="fas fa-camera me-2"></i>Change Photo
+                  </label>
+                </div>
+                <small class="text-muted">JPEG or PNG, max 2MB</small>
+              </div>
+            </div>
+            <!-- Personal Information Section -->
+            <div class="row mb-3">
+              <div class="col col-6 mb-2">
+                <form method="POST" action="{{ route('admins.update', $admin->id) }}">
+                  @csrf
+                  @method('PUT')
+
+                  <div class="row">
+                    <div class="col col-5 fw-bold">User Name:</div>
+                    <div class="col col-7 ">
+                      <input type="text" class="form-control sm py-0 text-start" name="userName" value="{{ old('userName', $admin->userName) }}"
+                        onchange="this.form.submit()" style="border:none;background-color:transparent;">
                     </div>
-                </div>
-          <!-- Personal Information Section -->
-          <div class="row mb-3">
-            <div class="col col-6 mb-2">
-              <form method="POST" action="{{ route('admins.update', $admin->id) }}">
-                @csrf
-                @method('PUT')
-
-                <div class="row">
-                  <div class="col col-4 fw-bold">User Name:</div>
-                  <div class="col col-8 text-start">
-                    <input type="text" class="form-control sm py-0" name="userName" value="{{ old('userName', $admin->userName) }}"
-                      onchange="this.form.submit()" style="border:none;background-color:transparent;">
                   </div>
-                </div>
-              </form>
-            </div>
-
-            <div class="col col-6 mb-2">
-              <form method="POST" action="{{ route('admins.update', $admin->id) }}">
-                @csrf
-                @method('PUT')
-
-                <div class="row">
-                  <div class="col col-4">Job Title:</div>
-                  <div class="col col-8">
-                    <input type="text" name="job_title" class="form-control sm py-0" value="{{ old('job_title', $admin->job_title) }}"
-                      onchange="this.form.submit()" style="border:none;background-color:transparent;">
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div class="col col-6 mb-2">
-              <form method="POST" action="{{ route('admins.update', $admin->id) }}">
-                @csrf
-                @method('PUT')
-
-                <div class="row">
-                  <div class="col col-6">Email:</div>
-                  <div class="col col-6">
-                    <input type="text" class="form-control sm py-0" name="email" value="{{ old('email', $admin->email) }}"
-                      onchange="this.form.submit()" style="border:none;background-color:transparent;">
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <div class="tab-pane fade" id="sessions" role="tabpanel" aria-labelledby="sessions-tab">
-          @if($admin->shifts->isNotEmpty())
-          <div class="row mt-2">
-            <div class="col col-12">
-              <div class="selected-products-container" style="font-size: 14px;">
-                <!-- Header Row -->
-                <div class="row g-0 border-bottom py-2  fw-bold align-items-center">
-                  <div class="col-1 text-center fw-bold fs-6">#</div>
-                  <div class="col-3 fw-bold fs-6">Session Name</div>
-                  <div class="col-3 text-center fw-bold fs-6">Start Time</div>
-                  <div class="col-3 text-center fw-bold fs-6">End Time</div>
-                  <div class="col-2 text-center fw-bold fs-6">Status</div>
-
-                </div>
-
-                <!-- Items Rows -->
-                @foreach($admin->shifts as $shift)
-                <div class="row g-0 border-bottom py-2 align-items-center">
-                  <div class="col-1 text-center fs-6">{{ $loop->iteration }}</div>
-                  <div class="col-3 ps-2 fs-6">{{ $shift->monybox->name }}</div>
-                  <div class="col-3 text-center fs-6">{{ $shift->start_time->format('d/m/Y H:i') }}</div>
-                  <div class="col-3 text-center fs-6">
-                    @if($shift->end_time)
-                    {{ $shift->end_time->format('d/m/Y H:i') }}
-                    @else
-                    <span class=" btn btn-sm btn-warning">In Progress</span>
-                    @endif
-                  </div>
-                  <div class="col-2 text-center fs-6"> @if($shift->status == 'Active')
-                    <span class="btn btn-sm btn-success">Active</span>
-                    @else
-                    <span class="btn btn-sm btn-secondary">Closed</span>
-                    @endif
-                  </div>
-
-
-
-                </div>
-                @endforeach
+                </form>
               </div>
 
+              <div class="col col-6 mb-2">
+                <form method="POST" action="{{ route('admins.update', $admin->id) }}">
+                  @csrf
+                  @method('PUT')
 
+                  <div class="row">
+                    <div class="col col-4">Job Title:</div>
+                    <div class="col col-8">
+                      <input type="text" name="job_title" class="form-control sm py-0" value="{{ old('job_title', $admin->job_title) }}"
+                        onchange="this.form.submit()" style="border:none;background-color:transparent;">
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="col col-6 mb-2">
+                <form method="POST" action="{{ route('admins.update', $admin->id) }}">
+                  @csrf
+                  @method('PUT')
 
-
+                  <div class="row">
+                    <div class="col col-6">Email:</div>
+                    <div class="col col-6">
+                      <input type="text" class="form-control sm py-0" name="email" value="{{ old('email', $admin->email) }}"
+                        onchange="this.form.submit()" style="border:none;background-color:transparent;">
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
+
           </div>
-          @else
-          <div class="alert alert-info mb-0">
-            No shifts found for this user
+        </div>
+  
+      <div class="tab-pane fade" id="sessions" role="tabpanel" aria-labelledby="sessions-tab">
+        @if($admin->shifts->isNotEmpty())
+        <div class="row mt-2">
+          <div class="col col-12">
+            <div class="selected-products-container" style="font-size: 14px;">
+              <!-- Header Row -->
+              <div class="row g-0 border-bottom py-2  fw-bold align-items-center">
+                <div class="col-1 text-center fw-bold fs-6">#</div>
+                <div class="col-3 fw-bold fs-6">Session Name</div>
+                <div class="col-3 text-center fw-bold fs-6">Start Time</div>
+                <div class="col-3 text-center fw-bold fs-6">End Time</div>
+                <div class="col-2 text-center fw-bold fs-6">Status</div>
+
+              </div>
+
+              <!-- Items Rows -->
+              @foreach($admin->shifts as $shift)
+              <div class="row g-0 border-bottom py-2 align-items-center">
+                <div class="col-1 text-center fs-6">{{ $loop->iteration }}</div>
+                <div class="col-3 ps-2 fs-6">{{ $shift->monybox->name }}</div>
+                <div class="col-3 text-center fs-6">{{ $shift->start_time->format('d/m/Y H:i') }}</div>
+                <div class="col-3 text-center fs-6">
+                  @if($shift->end_time)
+                  {{ $shift->end_time->format('d/m/Y H:i') }}
+                  @else
+                  <span class=" btn btn-sm btn-warning">In Progress</span>
+                  @endif
+                </div>
+                <div class="col-2 text-center fs-6"> @if($shift->status == 'Active')
+                  <span class="btn btn-sm btn-success">Active</span>
+                  @else
+                  <span class="btn btn-sm btn-secondary">Closed</span>
+                  @endif
+                </div>
+
+
+
+              </div>
+              @endforeach
+            </div>
+
+
+
+
           </div>
-          @endif
         </div>
-        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-          <p class="mt-3">....................</p>
+        @else
+        <div class="alert alert-info mb-0">
+          No shifts found for this user
         </div>
+        @endif
+      </div>
+      <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+        <p class="mt-3">....................</p>
       </div>
     </div>
   </div>
+</div>
 </div>
 
 @endsection
