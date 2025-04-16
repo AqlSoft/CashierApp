ecc80aa9ca6dd71dc2c2cf23015dd75f448b07f7
atefAkl <atefakl80@gmail.com>
2025-04-16 23:31:11 +0300

    Add kitchen order list view with print and status management buttons

--- storage/framework/views/c8e2cf805b6e04163a309e1ea559173a.php
+++ storage/framework/views/c8e2cf805b6e04163a309e1ea559173a.php
@@ -43,7 +43,7 @@
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                 <?php echo $__env->make('admin.kitchen.partials.order-details', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                                                 <?php echo $__env->make('admin.kitchen.partials.order-details', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
                                                 </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">إغلاق</button>
