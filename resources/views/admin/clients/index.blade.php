fe6ab6ba1baf270c303d2960ade88395d134a3e8
atefAkl <atefakl80@gmail.com>
2025-05-24 17:54:43 +0300

    feat: add client management interface with CRUD operations

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -2,92 +2,92 @@
 
 @section('contents')
 <div class="container-fluid">
+
+
+
+  <h1 class="mt-3 pb-2" style="border-bottom: 1px solid #dedede">{{ __('clients.client_list') }}</h1>
+  <!-- هنا سيتم اضافة العملاء -->
   <div class="row">
+    <div class="col col col-12">
+      <div class="card card-body w-100">
+        <form action="/admin/clients/store" method="POST">
+          @csrf
+          <div class="input-group sm mb-2">
+            <label class="input-group-text" for="vat_number">{{ __('clients.vat_number') }}</label>
+            <input type="number" class="form-control sm  @error('vat_number') is-invalid @enderror" name="vat_number" id="vat_number">
 
-    <div class="col-md-12">
-      <div id="products-container">
+            <label class="input-group-text" for="name">{{ __('clients.client_name') }}</label>
+            <input type="text" class="form-control sm @error('name') is-invalid @enderror" name="name" id="name">
 
-        <h1 class="mt-3 pb-2" style="border-bottom: 1px solid #dedede">{{ __('clients.client_list') }}</h1>
-        <!-- هنا سيتم اضافة العملاء -->
-        <div class="row">
-          <div class="col col col-12 pt-3">
-            <div class="card card-body">
-              <form action="/admin/clients/store" method="POST">
-                @csrf
-                <div class="input-group sm mb-2">
-                  <label class="input-group-text" for="vat_number">{{ __('clients.vat_number') }}</label>
-                  <input type="number" class="form-control sm  @error('vat_number') is-invalid @enderror" name="vat_number" id="vat_number">
-                  @error('vat_number')
-                  <div class="invalid-feedback">
-                    {{ $message }}
-                  </div>
-                  @enderror
-                  <label class="input-group-text" for="name">{{ __('clients.client_name') }}</label>
-                  <input type="text" class="form-control sm @error('name') is-invalid @enderror" name="name" id="name">
-                    @error('name')
-                  <span class="invalid-feedback">
-                    {{ $message }}
-</span>
-                  @enderror
-                  <label class="input-group-text" for="phone">{{ __('clients.phone_number') }}</label>
-                  <input type="number" class=" form-control sm @error('phone') is-invalid @enderror " name="phone" id="phone">
-                      @error('phone')
-                  <span class="invalid-feedback">
-                    {{ $message }}
-</span>
-                  @enderror
-                  <label class="input-group-text" for="address">{{ __('clients.address') }}</label>
-                  <input type="text" class="form-control sm" name="address" id="address">
-                  <button type="submit" class="py-0 btn btn-primary">{{ __('clients.save_client') }}</button>
-                  <button type="reset" class="py-0 btn btn-secondary" id="add-item">{{ __('clients.reset') }}</button>
-                </div>
-              </form>
-            </div>
+            <label class="input-group-text" for="phone">{{ __('clients.phone_number') }}</label>
+            <input type="number" class=" form-control sm @error('phone') is-invalid @enderror " name="phone" id="phone">
+
+            <label class="input-group-text" for="address">{{ __('clients.address') }}</label>
+            <input type="text" class="form-control sm" name="address" id="address">
+            <button type="submit" class="py-0 btn btn-primary">{{ __('clients.save_client') }}</button>
+            <button type="reset" class="py-0 btn btn-secondary" id="add-item">{{ __('clients.reset') }}</button>
           </div>
-          <div class="py-2" style="border-bottom: 1px solid #dedede"></div>
-        </div>
-        <!-- هنا سيتم عرض العملاء -->
-        <table class="table table-striped  mt-2">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>{{ __('clients.vat_number') }}</th>
-              <th>{{ __('clients.client_name') }}</th>
-              <th>{{ __('clients.phone_number') }}</th>
-              <th>{{ __('clients.address') }}</th>
-              <th>{{ __('clients.control') }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            @php $counter = 0; @endphp
-            @if (count($clients))
-            @foreach ($clients as $client)
-            <tr>
-              <td>{{ ++$counter }}</td>
-              <td>{{ $client->vat_number}}</td>
-              <td>{{ $client->name}}</td>
-              <td>{{ $client->phone }}</td>
-              <td>{{ $client->address }}</td>
-              <td>
-                <a class="btn btn-sm py-0" href="{{ route('view-client-info', $client->id) }}"><i
-                    class="fas fa-eye text-success" title="{{ __('clients.view_details') }}"></i></a>
-                <a class="btn btn-sm py-0" href="{{ route('edit-client-info', $client->id) }}"><i
-                    class="fa fa-edit text-primary" title="{{ __('clients.edit') }}"></i></a>
-                <a class="btn btn-sm py-0" onclick="if(!confirm('You are about to delete a order, are you sure!?.')){return false}"
-                  title="{{ __('clients.delete') }}" href="{{ route('destroy-client-info', $client->id) }}"><i
-                    class="fa fa-trash text-danger"></i></a>
-              </td>
-            </tr>
-            @endforeach
-            @else
-            <tr>
-              <td colspan="7">{{ __('clients.no_clients') }}</td>
-            </tr>
-            @endif
-          </tbody>
-        </table>
+          @error('phone')
+          <span class="invalid-feedback">
+            {{ $message }}
+          </span>
+          @enderror
+          @error('name')
+          <span class="invalid-feedback">
+            {{ $message }}
+          </span>
+          @enderror
+          @error('vat_number')
+          <span class="invalid-feedback">
+            {{ $message }}
+          </span>
+          @enderror
+        </form>
       </div>
     </div>
+    <div class="py-2" style="border-bottom: 1px solid #dedede"></div>
   </div>
+  <!-- هنا سيتم عرض العملاء -->
+  <table class="table table-striped  mt-2 w-100">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>{{ __('clients.vat_number') }}</th>
+        <th>{{ __('clients.client_name') }}</th>
+        <th>{{ __('clients.phone_number') }}</th>
+        <th>{{ __('clients.address') }}</th>
+        <th>{{ __('clients.control') }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      @php $counter = 0; @endphp
+      @if (count($clients))
+      @foreach ($clients as $client)
+      <tr>
+        <td>{{ ++$counter }}</td>
+        <td>{{ $client->vat_number}}</td>
+        <td>{{ $client->name}}</td>
+        <td>{{ $client->phone }}</td>
+        <td>{{ $client->address }}</td>
+        <td>
+          <a class="btn btn-sm py-0" href="{{ route('view-client-info', $client->id) }}"><i
+              class="fas fa-eye text-success" title="{{ __('clients.view_details') }}"></i></a>
+          <a class="btn btn-sm py-0" href="{{ route('edit-client-info', $client->id) }}"><i
+              class="fa fa-edit text-primary" title="{{ __('clients.edit') }}"></i></a>
+          <a class="btn btn-sm py-0" onclick="if(!confirm('You are about to delete a order, are you sure!?.')){return false}"
+            title="{{ __('clients.delete') }}" href="{{ route('destroy-client-info', $client->id) }}"><i
+              class="fa fa-trash text-danger"></i></a>
+        </td>
+      </tr>
+      @endforeach
+      @else
+      <tr>
+        <td colspan="7">{{ __('clients.no_clients') }}</td>
+      </tr>
+      @endif
+    </tbody>
+  </table>
+</div>
+
 </div>
 @endsection
